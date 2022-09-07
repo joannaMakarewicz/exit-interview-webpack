34c940575d9fc7205e9e134baad966920c13a670
Joanna Makarewicz <joanna.milun@gmail.com>
2022-09-07 13:18:43 +0200

    Dark mode option edded

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,25 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+import moment from 'moment/moment';
+
+const myDate=moment().format('MMMM Do YYYY, h:mm:ss a');
+const myDate2=document.querySelector('.header__date');
+myDate2.innerHTML=myDate;
+
+let isDark=false;
+const switchMode = document.querySelector('.header__dark--js');
+switchMode.addEventListener('click', () =>{
+    if(isDark) {
+        document.documentElement.style.setProperty('--background-color', 'white');
+        document.documentElement.style.setProperty('--text-color', 'black');
+        document.documentElement.style.setProperty('--background-article', '#d1e2d1');
+        isDark=false;
+    }else{
+        document.documentElement.style.setProperty('--background-color', 'black');
+        document.documentElement.style.setProperty('--text-color', 'white');
+        document.documentElement.style.setProperty('--background-article', '#386641');
+        isDark=true;
+    }
+})
+
